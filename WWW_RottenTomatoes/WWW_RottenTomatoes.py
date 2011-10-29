4e8fd1eebafc99ec736b0bbe1bf7923ead2082d3
caseyvega <casey.vega@gmail.com>
2011-10-29 08:28:06 +0000

    update functions and general cleanup/organization

--- WWW_RottenTomatoes/WWW_RottenTomatoes.py
+++ WWW_RottenTomatoes/WWW_RottenTomatoes.py
@@ -33,45 +33,38 @@ class WWW_RottenTomatoes(object):
         return json.loads(bytes.decode(response))
 
     """
-    The movies search endpoint for plain text queries.
+    Displays Top Box Office Earning Movies, Sorted by Most Recent Weekend Gross
+    Ticket Sales
 
-    @param: query (plain text), required
-    @param: page  (selected page of movie results), default 1
-    @param: limit (results per page), default 30
+    @param: limit (Limits the number of results returned), default 10
+    @param: country (localized country data (ISO 3166-1 alpha-2)), default US
     """
-    def MoviesSearch(self, query, page=None, limit=None):
-        self.params += '&q=' + urllib.parse.quote_plus(query)
-
-        if page is not None:
-            self.params += '&page=' + page
+    def BoxOfficeMovies(self, limit=None, country=None):
         if limit is not None:
             self.params += '&limit=' + limit
-
-        url = self.host + '/movies' + self.params
-
-        return self.__request__(url)
-    
-    """
-    Displays the top level (movie and dvd) lists available in the API.
-    """
-    def ListsDirectory(self):
-        url = self.host + '/lists' + self.params
-
+        if country is not None:
+            self.params += '&county=' + country 
+   
+        url = self.host + '/lists/movies/box_office' + self.params
+        
         return self.__request__(url)
 
     """
-    Shows the movie lists available.
-    """
-    def MovieListsDirectory(self):
-        url = self.host + '/lists/movies' + self.params
+    Retrieves movies currently in theaters
 
-        return self.__request__(url)
+    @param: country (localized country data (ISO 3166-1 alpha-2)), default US
+    @param: page (The selected page of upcoming movies), default 1
+    @param: page_limit (Opcoming movies to show per page), default 16 
+    """
+    def InTheaterMovies(self, country=None, page=None, page_limit=None):
+        if country is not None:
+            self.params += '&county=' + country
+        if page is not None:
+            self.params += '&page=' + page
+        if page_limit is not None:
+            self.params += '&page_limit=' + page_limit
 
-    """
-    Shows the DVD lists available.
-    """
-    def DvdsListsDirectory(self):
-        url = self.host + '/lists/dvds' + self.params
+        url = self.host + '/lists/movies/in_theaters' + self.params
 
         return self.__request__(url)
 
@@ -112,6 +105,42 @@ class WWW_RottenTomatoes(object):
         return self.__request__(url)
 
     """
+    Retrieves the current top dvd rentals
+    
+    @param: limit (Limits the number of results returned), default 16
+    @param: country (localized country data (ISO 3166-1 alpha-2)), default US    
+    """
+    def TopRentalsDvds(self, limit=None, country=None):
+        if limit is not None:
+            self.params += '&limit=' + limit
+        if country is not None:
+            self.params += '&county=' + country
+
+        url = self.host + '/lists/dvds/top_rentals' + self.params
+
+        return self.__request__(url)
+
+    """
+    Retrieves current release dvds. Results are paginated if they go past the
+    specified page limit
+    
+    @param: country (localized country data (ISO 3166-1 alpha-2)), default US
+    @param: page (The selected page of upcoming movies), default 1
+    @param: page_limit (Opcoming movies to show per page), default 16
+    """
+    def CurrentReleaseDvds(self, country=None, page=None, page_limit=None):
+        if country is not None:
+            self.params += '&county=' + country
+        if page is not None:
+            self.params += '&page=' + page
+        if page_limit is not None:
+            self.params += '&page_limit=' + page_limit
+
+        url = self.host + '/lists/dvds/current_releases' + self.params
+
+        return self.__request__(url)
+
+    """
     Retrieves new release dvds. Results are paginated if they go past the
     specified page limit.
 
@@ -132,13 +161,33 @@ class WWW_RottenTomatoes(object):
         return self.__request__(url)
 
     """
+    Retrieves upcoming dvds. Results are paginated if they go past the specified
+    page limit
+
+    @param: country (localized country data (ISO 3166-1 alpha-2)), default US
+    @param: page (The selected page of upcoming movies), default 1
+    @param: page_limit (Opcoming movies to show per page), default 16
+    """
+    def UpcomingDvds(self, country=None, page=None, page_limit=None):
+        if country is not None:
+            self.params += '&county=' + country
+        if page is not None:
+            self.params += '&page=' + page
+        if page_limit is not None:
+            self.params += '&page_limit=' + page_limit
+
+        url = self.host + '/lists/dvds/upcoming' + self.params
+
+        return self.__request__(url)
+
+    """
     Detailed information on a specific movie specified by Id. You can use the
     movies search endpoint or peruse the lists of movies/dvds to get the urls
     to movies.
 
     @param: movie_id (The id number of a specific movie), required
     """
-    def MovieInfo(self, movie_id):
+    def MoviesInfo(self, movie_id):
         url = self.host + '/movies/' + movie_id + self.params
 
         return self.__request__(url)
@@ -148,8 +197,18 @@ class WWW_RottenTomatoes(object):
 
     @param: movie_id (The id number of a specific movie), required
     """
-    def MovieCast(self, movie_id):
+    def MoviesCast(self, movie_id):
         url = self.host + '/movies/' + movie_id + '/cast' + self.params
+
+        return self.__request__(url)
+
+    """
+    Pulls the complete movie cast for a movie
+
+    @param: movie_id (The id number of a specific movie), required
+    """
+    def MoviesClips(self, movie_id):
+        url = self.host + '/movies/' + movie_id + '/clips' + self.params
 
         return self.__request__(url)
 
@@ -168,7 +227,7 @@ class WWW_RottenTomatoes(object):
                         "dvd" pulls the reviews given on the DVD of the movie.
                         "all" as the name implies retrieves all reviews.)
     """
-    def MovieReviews(self, movie_id, review_type=None, country=None, page=None, page_limit=None):
+    def MoviesReviews(self, movie_id, review_type=None, country=None, page=None, page_limit=None):
         if review_type is not None:
             self.params += '&review_type=' + review_type
         if country is not None:
@@ -197,33 +256,60 @@ class WWW_RottenTomatoes(object):
         return self.__request__(url)
 
     """
-    Retrieves movies currently in theaters
+    Provides a movie lookup by an id from a different vendor. Only supports imdb
+    lookup at this time
 
-    @param: country (localized country data (ISO 3166-1 alpha-2)), default US
-    @param: page (The selected page of upcoming movies), default 1
-    @param: page_limit (Opcoming movies to show per page), default 16 
+    @param: movie_id (The id number of a specific movie), required
+    @param: type (alias type to look up - only imdb is supported at this time)
     """
-    def InTheaterMovies(self, country=None, page=None, page_limit=None):
-        if country is not None:
-            self.params += '&county=' + country
-        if page is not None:
-            self.params += '&page=' + page
-        if page_limit is not None:
-            self.params += '&page_limit=' + page_limit
-
-        url = self.host + '/lists/movies/in_theaters' + self.params
+    def MoviesAlias(self, movie_id, alias_type):
+        
+        self.params += '&type=' + alias_type
+        self.params += '&id' + movie_id
+        
+        url = self.host + '/movie_alias/' + self.params
 
         return self.__request__(url)
 
     """
-    The API supports JSONP calls. Simply append a callback parameter with the
-    name of your callback method at the end of the request.
+    The movies search endpoint for plain text queries.
 
-    @param: callback_fn (name of callback function), required
+    @param: query (plain text), required
+    @param: page  (selected page of movie results), default 1
+    @param: limit (results per page), default 30
     """
-    def CallBack(self, callback_fn):
-        self.params += '&callback=' + callback_fn
-        
-        url = self.host + self.params
+    def MoviesSearch(self, query, page=None, limit=None):
+        self.params += '&q=' + urllib.parse.quote_plus(query)
+
+        if page is not None:
+            self.params += '&page=' + page
+        if limit is not None:
+            self.params += '&limit=' + limit
+
+        url = self.host + '/movies' + self.params
+
+        return self.__request__(url)
+    
+    """
+    Displays the top level (movie and dvd) lists available in the API.
+    """
+    def ListsDirectory(self):
+        url = self.host + '/lists' + self.params
+
+        return self.__request__(url)
+
+    """
+    Shows the movie lists available.
+    """
+    def MovieListsDirectory(self):
+        url = self.host + '/lists/movies' + self.params
+
+        return self.__request__(url)
+
+    """
+    Shows the DVD lists available.
+    """
+    def DvdsListsDirectory(self):
+        url = self.host + '/lists/dvds' + self.params
 
         return self.__request__(url)
